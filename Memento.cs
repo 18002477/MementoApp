f553398212414e57e4e73fd73d070c649d4bcd10
Sandile-Cele <Sandilecele101@gmail.com>
2021-04-03 16:16:20 +0200

    +: memento class create

---
+++ Memento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniTexteditor
+{
+    class Memento
+    {
+        string word;
+
+        public Memento(string word)
+        {
+            this.word = word;
+        }
+
+        public string Word()
+        {
+            return word;
+        }
+    }
+}
